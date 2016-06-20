b06d56fc1e22b927ef3abe18bbaa49174b47db41
xudaile <932836534@qq.com>
2016-06-20 10:05:36 +0800

    ddd
    sddddss

--- src/nds/weixin/ext/dispose/ClickDispose.java
+++ src/nds/weixin/ext/dispose/ClickDispose.java
@@ -84,6 +84,7 @@ public class ClickDispose implements IMessageDispose {
 			event.setParameter("command", Content);
 
 			//YYfffsssss
+			//ddd
 			try {
 				vh=controller.handleEvent(event);
 			}catch(Exception e) {
